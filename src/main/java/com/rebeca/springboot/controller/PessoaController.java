aae7b422aa9f4591831d13f9b2a68854d871e6bc
Rebeca Cruz <rebecacruz@me.com>
2020-07-02 21:03:18 -0300

    Realizando validacao, validando telefone

--- src/main/java/com/rebeca/springboot/controller/PessoaController.java
+++ src/main/java/com/rebeca/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package com.rebeca.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +42,23 @@ public class PessoaController {
 		}
 		
 		@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
-		public ModelAndView salvar(Pessoa pessoa) {
+		public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+			
+			if (bindingResult.hasErrors()) {
+				ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+				Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
+				modelAndView.addObject("pessoas", pessoaIt);
+				modelAndView.addObject("pessoaobj", pessoa);
+				
+				List<String> msg = new ArrayList<String>();
+				for (ObjectError objectErro : bindingResult.getAllErrors()) {
+					msg.add(objectErro.getDefaultMessage()); //vem das anotações @NotEmpty e outras
+				}
+				
+				modelAndView.addObject("msg", msg);
+				return modelAndView;
+			}
+			
 			pessoaRepository.save(pessoa);
 		
 			ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
@@ -96,13 +118,33 @@ public class PessoaController {
 		public ModelAndView addFonePessoa(Telefone telefone, @PathVariable("pessoaid") Long pessoaid) {
 		
 			Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
+			
+			if (telefone != null && telefone.getNumero().isEmpty()
+					|| telefone.getTipo().isEmpty()) {
+						
+				ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+				modelAndView.addObject("pessoaobj", pessoa);
+				modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+						
+				List<String> msg = new ArrayList<String>();
+				if (telefone.getNumero().isEmpty()){
+					msg.add("Numero deve ser informado");
+				}
+				if (telefone.getTipo().isEmpty()) {
+					msg.add("Tipo deve ser informado");
+				}
+				modelAndView.addObject("msg", msg);
+				
+				return modelAndView;
+			}
+			
+				
 			telefone.setPessoa(pessoa);
-			
 			telefoneRepository.save(telefone);
-			
 			ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 			modelAndView.addObject("pessoaobj", pessoa);
 			modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+			
 			return modelAndView;	
 		}
 		
--- src/main/java/com/rebeca/springboot/model/Pessoa.java
+++ src/main/java/com/rebeca/springboot/model/Pessoa.java
@@ -9,6 +9,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
 
 @Entity
 public class Pessoa implements Serializable {
@@ -18,10 +23,15 @@ public class Pessoa implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@NotNull(message = "Nome não pode ser nulo")
+	@NotEmpty(message="Nome não pode ser vazio")
 	private String nome;
 	
+	@NotNull(message = "Sobrenome não pode ser nulo")
+	@NotEmpty(message="Sobrenome não pode ser vazio")
 	private String sobrenome;
 	
+	@Min(value = 18, message = "idade inválida")
 	private String idade;
 	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)

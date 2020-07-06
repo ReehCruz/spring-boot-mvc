83f6c4ef5a01708d0bcfcca88279b7f0afd28a5d
Rebeca Cruz <rebecacruz@me.com>
2020-07-06 20:33:31 -0300

    Spring Security, configurando em memoria

--- src/main/java/com/rebeca/springboot/SpringbootApplication.java
+++ src/main/java/com/rebeca/springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package com.rebeca.springboot;
 
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -16,6 +18,12 @@ public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+
+		
+//		Quando quiser cripto na pag
+//		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+//		String result = enconder.encode("root");
+//		System.out.println(result);
 	}
 
 }
---
+++ src/main/java/com/rebeca/springboot/security/WebConfigSecurity.java
@@ -0,0 +1,43 @@
+package com.rebeca.springboot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Override //configura as solicitações de acesso por http 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable() //desativa as configurações padrão de memória
+		.authorizeRequests() //permiti restringir acessos 
+		.antMatchers(HttpMethod.GET, "/").permitAll() //qualqur usuario acessa a pagina inicial 
+		.anyRequest().authenticated() 
+		.and().formLogin().permitAll() // permite qlqr usuario
+		.and().logout() // Mapeia URL de logout e invalida usuario autentificado 
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+	}
+	
+	
+	@Override //cria atentificaçao do usuario com banco ou em memória 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+		.withUser("root")
+		.password("$2a$10$9GZJVZmeMCPJXvPyKqAFJOQEeRe/xTo6XIBmMPYYaYGknA5H1kunG")
+		.roles("ADMIN");
+	} 
+	
+	@Override //ignora URL especificas
+	public void configure(WebSecurity web) throws Exception {
+	//	web.ignoring().antMatchers("/materialize/**");
+	}
+}
